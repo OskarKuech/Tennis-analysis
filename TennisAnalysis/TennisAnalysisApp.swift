d3a1f19af0ca91bd3290838753706a918fc73cc1
Oskar Kuech <oskue@gmx.de>
2023-10-06 14:55:11 +0200

    Initial Commit

---
+++ TennisAnalysis/TennisAnalysisApp.swift
@@ -0,0 +1,17 @@
+//
+//  TennisAnalysisApp.swift
+//  TennisAnalysis
+//
+//  Created by Oskar Küch on 06.10.23.
+//
+
+import SwiftUI
+
+@main
+struct TennisAnalysisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
